dab5a21ab2270b8a2ea269448432b80668d1e885
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-14 10:42:13 +0900

    新規作成

---
+++ src/components/UserPost.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <p>ユーザー情報</p>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'bookDetail',
+  data() {
+    return {
+        
+    }
+  },
+  props: {
+    id     :Number,
+    title  :String,
+    content:String
+  },
+  methods: {
+    
+  },
+  created() {
+    if(this.$route.params.id > 4){ this.$router.push('/book') }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
